86fce696fb75a5d868d09605b122122a2ea01c59
IndraHardika <92752408+KZdra@users.noreply.github.com>
2025-03-03 20:59:29 +0700

    Nyieun helper Jeung SwallaAlert

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,7 @@ import "./bootstrap";
 import "./datatable";
 import { apiService } from "./apiservice";
 import { MakeChart } from "./chart";
-
+import * as SwalHelper from "./swalHelper";
 window.MakeChart = MakeChart;
 window.apiService = apiService;
+window.SwalHelper = SwalHelper;
